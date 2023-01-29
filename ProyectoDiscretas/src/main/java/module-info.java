83ec6beadfd86dda34519d2bb1229021cc7d5254
CarlosAlvia <107290198+CarlosAlvia@users.noreply.github.com>
2023-01-29 00:08:44 -0500

    Delete ProyectoDiscretas directory

--- ProyectoDiscretas/src/main/java/module-info.java
+++
@@ -1,8 +0,0 @@
-module com.mycompany.proyectodiscretas {
-    requires javafx.controls;
-    requires javafx.fxml;
-    requires java.base;
-
-    opens com.mycompany.proyectodiscretas to javafx.fxml;
-    exports com.mycompany.proyectodiscretas;
-}
